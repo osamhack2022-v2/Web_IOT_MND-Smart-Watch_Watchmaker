86a73514a698860f3e548e1df9b2178cd79b6287
1Park <39184453+1Park@users.noreply.github.com>
2022-10-23 11:06:16 +0000

    some changes

---
+++ WEB(BE)/routes/update.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var updateUser = require('../sqlite_update'); 
+
+router.post('/', function(req, res, next) {
+
+    console.log("get update request")
+    //updateUser.updateUserInfo("22-70007066",null,null,null,null,"근무Off","생활관",null);
+    updateUser.updateUserInfo(req.body.number,req.body.name,req.body.belong,req.body.rank ,req.body.new_number,req.body.working,req.body.location,null);
+    console.log("DB updated.");
+    res.send("FE responsed")
+    
+  });
+
+
+module.exports = router;
---
+++ WEB(BE)/sqlite_update.js
@@ -0,0 +1,49 @@
+
+// require 했을때 users.json이 정상적으로 write 되지 않음
+module.exports.updateUserInfo = async function (number, name, belong, rank, new_number, working, location, heartrate){
+
+var sqlite3 = require('sqlite3').verbose(); 
+const fs = require('fs');
+const path = require('path');
+const dbPath = path.resolve(__dirname, './db/UserDatabase.db');
+  
+
+let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+        console.error(dbPath);
+    } else {
+        console.log('Connected to the UserDatabase.');
+    }
+});
+
+
+const queries = [];
+const vars = [ ["name",name], ["belong",belong], ["rank",rank], ["number",new_number], ["working",working], ["location",location], ["heartrate",heartrate]];
+console.log("update operating. . . . .")
+vars.forEach((value)=>{
+
+  if(value[1]!=undefined){
+    queries.push( `UPDATE Users SET `+value[0]+`="`+value[1]+`" WHERE number="`+number+`";` )
+  }
+});
+console.log("======update operating======")
+console.log(queries)
+db.serialize(() => {
+  queries.forEach((query) => db.all(query,[],(err) =>{  if(err){ throw err; } } ) ); 
+
+});
+  
+
+db.close((err) =>{
+  if(err){
+    console.error(err.message);
+  }
+  console.log('Close the database connection.');
+}); 
+
+
+
+}
+
+//node sqlite-to_json.js 로 실행
