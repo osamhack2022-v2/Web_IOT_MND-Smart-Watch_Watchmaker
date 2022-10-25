6f0baebdb17fe6a4fdaaa279068b3b084a842813
S4nso <71062855+S4nso@users.noreply.github.com>
2022-10-25 14:21:56 +0000

    notice edit

--- WEB(FE)/src/views/Notice.vue
+++ WEB(FE)/src/views/Notice.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-8">
-        <div class="row mt-4">
-          <div class="col-xl-12 mb-xl-0 mb-4">
-            <output-card />
-          </div>
-        </div>
-        <div class="row mt-4">
-          <div class="col-xl-12 mb-xl-0 mb-4">
-            <input-card />
-          </div>
-        </div>
+      <div class="col-lg-6">
+        <output-card />
       </div>
+      <div class="col-lg-6">
+        <input-card />
+      </div>
+      <!--
       <div class="col-lg-4">
         <invoice-card class="mt-4" />
       </div>
+      -->
     </div>
   </div>
 
@@ -23,14 +19,13 @@
 
 <script>
 
-import InvoiceCard from "./components/InvoiceCard.vue";
+//import InvoiceCard from "./components/InvoiceCard.vue";
 import OutputCard from "./components/OutputCard.vue";
 import InputCard from "./components/Inputcard.vue";
 
 export default {
   name: "Billing",
   components: {
-    InvoiceCard,
     OutputCard,
     InputCard,
   },
